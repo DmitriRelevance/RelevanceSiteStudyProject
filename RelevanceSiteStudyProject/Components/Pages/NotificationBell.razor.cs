3d92ff093dc1133547de419347f098200d80d1d6
Dmitri Zaruba <dmitri.zaruba@rlvmgt.onmicrosoft.com>
2025-07-25 15:43:55 +0300

    Reviewed the Bell's notification

--- RelevanceSiteStudyProject/Components/Pages/NotificationBell.razor.cs
+++ RelevanceSiteStudyProject/Components/Pages/NotificationBell.razor.cs
@@ -1,25 +1,58 @@
-﻿namespace RelevanceSiteStudyProject.Components.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace RelevanceSiteStudyProject.Components.Pages
 {
     public partial class NotificationBell
     {
 
         private bool showPanel = false;
 
+        private bool pendingElementReferenteRegistration = false;
+        private ElementReference panelRef;
+        private DotNetObjectReference<NotificationBell>? objRef;
         protected override void OnInitialized()
         {
             NotificationService.OnChange += StateHasChanged;
         }
 
-        private void TogglePanel()
+        protected override async Task OnAfterRenderAsync(bool firstRender)
         {
+            if(pendingElementReferenteRegistration && showPanel)
+            {
+                pendingElementReferenteRegistration = false;
+                objRef = DotNetObjectReference.Create(this);
+                await JSRuntime.InvokeVoidAsync("notificationUtils.registerOutsideClickById", "notificationPanel", objRef);
+            }
+        }
+
+        private async Task TogglePanel()
+        {
+            // Toggle the visibility of the notification panel
             showPanel = !showPanel;
 
             // if (showPanel)
             //     NotificationService.MarkAllAsRead();
+
+            if (showPanel)
+            {
+                pendingElementReferenteRegistration = true;
+            }
+        }
+
+        [JSInvokable]
+        public void HidePanel()
+        {
+            showPanel = false;
+            StateHasChanged();
         }
 
         private void MarkAllAsRead() => NotificationService.MarkAllAsRead();
 
-        public void Dispose() => NotificationService.OnChange -= StateHasChanged;
+        public void Dispose()
+        {
+            NotificationService.OnChange -= StateHasChanged;
+            objRef?.Dispose();
+        }
     }
 }
---
+++ RelevanceSiteStudyProject/wwwroot/js/notifications.js
@@ -0,0 +1,18 @@
+﻿window.notificationUtils = {
+    registerOutsideClickById: function (elementId, dotNetHelper) {
+        const element = document.getElementById(elementId);
+
+        if (!element) return;
+
+        function onClick(event) {
+            if (!element.contains(event.target)) {
+                dotNetHelper.invokeMethodAsync("HidePanel");
+                document.removeEventListener("click", onClick);
+            }
+        }
+
+        setTimeout(() => {
+            document.addEventListener("click", onClick);
+        }, 100);
+    }
+};
