6137860159bf6fc78dbaddef68bb82f33287cf97
Dmitri Zaruba <dmitri.zaruba@rlvmgt.onmicrosoft.com>
2025-07-31 13:07:41 +0300

    Implemented validationFilter for API

---
+++ RelevanceSiteStudyProject.API/Filters/ValidationFilter.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RelevanceSiteStudyProject.API.Filters
+{
+    public class ValidationFilter<T> : IEndpointFilter where T : class
+    {
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            var dto = context.Arguments.OfType<T>().FirstOrDefault();
+            if (dto is null)
+                return Results.BadRequest("Invalid request body.");
+
+            var validationContext = new ValidationContext(dto);
+            var results = new List<ValidationResult>();
+
+            if (!Validator.TryValidateObject(dto, validationContext, results, validateAllProperties: true))
+            {
+                var errorDict = results
+                    .GroupBy(e => e.MemberNames.FirstOrDefault() ?? "")
+                    .ToDictionary(g => g.Key, g => g.Select(r => r.ErrorMessage).ToArray());
+
+                return Results.ValidationProblem(errorDict);
+            }
+
+            return await next(context);
+        }
+    }
+
+}
